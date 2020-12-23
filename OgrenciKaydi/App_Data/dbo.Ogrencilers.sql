06b8ba5454183546db8c84d05d3ded30dc389885
Ahmet Reçber <ahmetrecber1835@gmail.com>
2020-12-23 18:21:50 +0300

    .

---
+++ OgrenciKaydi/App_Data/dbo.Ogrencilers.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Ogrencilers] (
+    [ID]     INT            IDENTITY (1, 1) NOT NULL,
+    [OkulNo] NVARCHAR (20) NOT NULL,
+    [Adi]    NVARCHAR (20) NOT NULL,
+    [Soyadi] NVARCHAR (20) NOT NULL,
+    [Bolum]  NVARCHAR (20) NOT NULL,
+    CONSTRAINT [PK_dbo.Ogrencilers] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
---
+++ OgrenciKaydi/Models/Ogrenciler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace OgrenciKaydi.Models
+{
+    public class Ogrenciler
+    {
+        public int ID  { get; set; }
+        
+        [Required]
+        public string OkulNo { get; set; }
+        [Required]
+        public string Adi { get; set; }
+        [Required]
+        public string Soyadi { get; set; }
+        [Required]
+        public string Bolum { get; set; }
+    }
+}
